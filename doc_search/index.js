d7b87a8a881fab8df7765f8a8083b74218ca434e
jeferson <jeff.lemos.a@gmail.com>
2023-02-17 07:21:55 -0300

    document search minimally viable

--- doc_search/index.js
+++ doc_search/index.js
@@ -1,38 +1,23 @@
 'use strict'
 
-var text;
-
 var run = require('sync-runner');
+var Search_OP = require("flexsearch");
 
-const searchFile = (file, search) => {
+function searchFile (file, search){
 
-  let text = run("java -jar lib/tika-app-2.7.0.jar --help")
-  //const parser = java.importClass('org.apache.tika.parser');
+  let text = run(`java -jar lib/tika-app-2.7.0.jar -t ${file}`);
 
-    // all code bellow using Tika was considering "node-tika" package, that I decided to avoid
-    // in favour of running Tika on java directly, it is there just for future refference
-	//var Tika = require('node-tika');
-	//var conversor = new Tika();
+  const text_lines = text.split("\n");
+  let search_index = new Search_OP.Index();
 
-	//var Search_OP = require("flexsearch");
+  for (let i =0; i < text_lines.length; i++ ) search_index.add((i+1),text_lines[i]);
 
-	//var search_index = new Search_OP.Index();
-
-	//file = ( conversor.text(file,null, function(err, text) { return text; }) );
-	/*var parser = conversor.parse(file);
-	parser.on('text', function(data) {
-  		file = data;
-	});*/
-
-	//search_index.add(file);
-	//return search_index.search(search);
-
-   return text;
+  return search_index.search(search);
 }
 
 module.exports = {searchFile};
 
-var file = (process.argv[1]);
-var search = (process.argv[2]);
+var file = (process.argv[2]);
+var search = (process.argv[3]);
 
 console.log( searchFile(file,search) );
